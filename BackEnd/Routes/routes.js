bbbb217b45e6646acd1b8a4efb33ca13c8adeb6b
K <kad9603@gmail.com>
2021-01-22 13:57:34 -0500

    added /addreview post route

--- BackEnd/Routes/routes.js
+++ BackEnd/Routes/routes.js
@@ -7,4 +7,20 @@ router.get('/', (req, res) => {
     res.send('Hello from SkateLog backend!');
 });
 
+router.post('/addreview', (req, res) => {
+    const addedReview = new addReviewTemplate({
+        name: req.body.name,
+        msg: req.body.msg
+    });
+    addedReview.save()
+    .then(data => {
+        res.json(data);
+        res.send(`${ addedReview.name } left a review.`);
+    })
+    .catch(err => {
+        res.json(err);
+        res.send(`There was an error adding ${ addedReview.name }'s review.`)
+    })
+});
+
 module.exports = router;
