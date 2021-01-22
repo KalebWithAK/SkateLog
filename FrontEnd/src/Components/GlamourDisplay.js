dda82af565723c2e450a8703ef9bf6da4cde1b72
K <kad9603@gmail.com>
2021-01-22 13:57:48 -0500

    changed img border color to purple

--- FrontEnd/src/Components/GlamourDisplay.js
+++ FrontEnd/src/Components/GlamourDisplay.js
@@ -5,7 +5,7 @@ export default class GlamourDisplay extends React.Component {
         return (
             <div style={{ marginTop: '3em', marginBottom: '3em' }}>
                 <p>{this.props.text}</p>
-                <img style={{ height: '35em', width: '75em', border: 'solid gray 1px', borderRadius: '0.5em' }} src={ this.props.img }></img>
+                <img style={{ height: '35em', width: '75em', border: 'solid #7e3ff2 1px', borderRadius: '0.5em' }} src={ this.props.img }></img>
             </div>
         );
     }
